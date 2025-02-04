1f04dbd8522970adcf09e7829b86e4bafa1ace65
Elahi-Ehtesham <135419806+Elahi-Ehtesham@users.noreply.github.com>
2025-02-04 10:37:51 +0600

    Add files via upload

---
+++ Secant.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const double dif = 1e-6;
+
+class Secant{
+    public:
+    double f(double x){
+        return x*x*x*x -x - 10;
+      //  return x*x*x*x + x*x - 2;
+    }
+
+    double secant(double x1, double x2){
+        cout << x1 << " " << x2 << endl;
+        double root;
+        while (abs(x1 - x2) > dif) {
+            root = (x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1));
+          //  cout << root << endl;
+            x1 = x2;
+            x2 = root;
+        }
+        return root;
+    }
+};
+
+int main()
+{
+    cout << fixed << setprecision(6);
+    srand(time(0));
+
+    Secant sec;
+    double x1 = (rand()%201-100)/10.0, x2=(rand()%201-100)/10.0;
+    //cin >> x1 >> x2;
+    if(x1>x2)
+    swap(x1,x2);
+    double ans = sec.secant(x1, x2);
+    cout << ans << endl;
+    double root = rand();
+
+    return 0;
+}
